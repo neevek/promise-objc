94cdb020a8e42846efc22852dd7abae3416865c2
neevek <i@neevek.net>
2016-11-05 19:45:07 +0800

    Initial Commit

---
+++ promise-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  promise-objc
+//
+//  Created by 陈小黑 on 05/11/2016.
+//  Copyright © 2016 neevek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
